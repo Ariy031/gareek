6e2b81cbaaff2440808933eea73e2a4f021188b0
Ariy031 <muhmalikazhari@gmail.com>
2023-09-26 06:58:50 +0700

    ariy garek

---
+++ tugas_besar_pemerograman.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+    
+    int a,b,tambah,kali,kurang,bagi;
+
+cout<<"======================"<<endl;
+cout<<"PROGRAM KALKULATOR C++"<<endl;
+cout<<"======================"<<endl;
+
+cout<<"MASUKAN NILAI A : ";cin>>a;
+cout<<"MASUKAN NILAI B : ";cin>>b;
+
+tambah = a + b;
+kali   = a * b;
+kurang = a - b;
+bagi   = a / b;
+
+cout<<"======================"<<endl;
+cout<<"hasil dari tambah : "<<tambah<<endl;
+cout<<"hasil dari kali   : "<<kali<<endl;
+cout<<"hasil dari kurang : "<<kurang<<endl;
+cout<<"hasil dari bagi   : "<<bagi<<endl;
+
+return 0;
+
+}
